ddb38c29ea6c0723c019cce351af8606c4828f9f
juliandomeneghini <73919470+juliandomeneghini@users.noreply.github.com>
2023-06-09 08:07:39 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="image_processing-test",
-    version="0.0.1",
+    name="image-processing-package-test",
+    version="0.0.9.1",
     author="Julian Domeneghini",
     description="Test version - Image processing. This project belongs to Karina Tiemi Kato, Tech Lead, Machine Learning Engineer, Data Scientist Specialist at Take. This package is a demo for simulation of upload on the Test Pypi website, and it's from class of the Bootcamp developer full stack Python. E-mail:karinatkato@gmail.com.",
     long_description=page_description,
     long_description_content_type="text/markdown",
     url="https://github.com/juliandomeneghini/image-processing-package",
-    packages=['image_processing_package'],
+    packages=['image-processing-package-test'],
     install_requires=requirements,
     python_requires='>=3.8',
 )
